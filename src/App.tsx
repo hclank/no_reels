e34b04f03081166dd7539dfac1cd993e3338bb70
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-05-23 14:38:17 +0530

    forgot to add ts lmao

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+    <div className="w-[120vh] flex flex-col items-center justify-center h-screen">
+      <div className="p-16 flex justify-center items-center flex-col">
+        <img className="w-22 h-22" src="/logo.png" alt="logo" />
+        <h3 className="text-md m-2 font-bold">No Reels</h3>
+      </div>
+      <div className="footer p-4">
+        made by{" "}
+        <a href="https://github.com/hclank" target="_blank">
+          @hclank
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
