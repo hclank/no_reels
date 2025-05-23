dd89b3f995dadc6ade38326dd52bea4efd52a8d0
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-05-23 15:03:21 +0530

    getting instagram page

---
+++ src/contentScript.ts
@@ -0,0 +1,2 @@
+const html = document.documentElement.outerHTML;
+console.log(html);
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        contentScript: resolve(__dirname, "src/contentScript.ts"),
+      },
+      output: {
+        entryFileNames: "[name].js",
+      },
+    },
+    outDir: "dist",
+  },
+  base: "./",
 });
